3e67a52dbe44cbaa1d58cd46780a81e01b386dc0
Greizgh <greizgh@ephax.org>
2019-03-26 18:52:45 +0100

    Generate EnvFile from bitwarden_rs template

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SRC="$DIR/git"
 DST="$DIR/dist"
 
+# Clone bitwarden_rs
 if [ -d "$SRC" ]; then
   cd "$SRC" || exit
   git pull
@@ -13,6 +14,13 @@ if [ -d "$SRC" ]; then
 else
   git clone https://github.com/dani-garcia/bitwarden_rs.git "$SRC"
 fi
+
+# Prepare EnvFile
+CONFIG="$DIR/debian/config.env"
+cp "$SRC/.env.template" "$CONFIG"
+sed -i "s#\# DATA_FOLDER=data#DATA_FOLDER=/var/lib/bitwarden_rs#" "$CONFIG"
+sed -i "s#\# WEB_VAULT_FOLDER=web-vault/#WEB_VAULT_FOLDER=/usr/share/bitwarden_rs/web-vault/#" "$CONFIG"
+
 mkdir -p "$DST"
 
 docker build -t bitwarden-deb "$DIR"
