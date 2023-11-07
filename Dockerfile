6b6d0f0b0956d9e1f914c623e0ecb77f16a44396
Greizgh <greizgh@ephax.org>
2023-11-07 20:42:18 +0100

    Build package from release docker image
    This is a radical change in the approach, which come with several
    improvements. There is no patching of upstream Dockerfiles anymore,
    this greatly reduces coupling. Also, there is no local build, which
    leads to much faster package building.
    
    I believe this is the way forward if we don't want to spend too much
    time maintaining this.
    It comes at a small cost: there is no building under the previous name.

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+ARG ref
+FROM vaultwarden/server:$ref
+RUN mkdir /outdir
+RUN mkdir -p /vaultwarden_package/DEBIAN
+RUN mkdir -p /vaultwarden_package/usr/local/bin
+RUN mkdir -p /vaultwarden_package/usr/lib/systemd/system
+RUN mkdir -p /vaultwarden_package/etc/vaultwarden
+RUN mkdir -p /vaultwarden_package/usr/share/vaultwarden
+
+WORKDIR /vaultwarden_package
+COPY debian/control /vaultwarden_package/DEBIAN/control
+COPY debian/postinst /vaultwarden_package/DEBIAN/postinst
+COPY debian/conffiles /vaultwarden_package/DEBIAN/conffiles
+COPY debian/config.env /vaultwarden_package/etc/vaultwarden
+COPY debian/vaultwarden.service /vaultwarden_package/usr/lib/systemd/system
+
+RUN cp -r /web-vault /vaultwarden_package/usr/share/vaultwarden \
+    && cp /vaultwarden /vaultwarden_package/usr/local/bin/
+
+CMD dpkg-deb --build . /outdir/vaultwarden.deb
--- build.sh
+++ build.sh
@@ -1,37 +1,23 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-SRC="$DIR/git"
 DST="$DIR/dist"
+REF=""
+RECOMMENDS=""
 
-OS_VERSION_NAME="bullseye"
 DB_TYPE="sqlite"
 SYSTEMD_DB="true"
-ARCH_DIR="amd64"
-PACKAGENAME="vaultwarden"
-PACKAGEDIR="vaultwarden"
-SERVICEUSER="vaultwarden"
-EXECUTABLENAME="$PACKAGEDIR"
+ARCH="amd64"
 
-while getopts ":r:o:d:a:p:i:u:e:s" opt; do
+while getopts ":r:d:a:s" opt; do
   case $opt in
     r) REF="$OPTARG"
     ;;
-    o) OS_VERSION_NAME="$OPTARG"
-    ;;
     d) DB_TYPE="$OPTARG"
     ;;
-    a) ARCH_DIR="$OPTARG"
-    ;;
-    p) PACKAGENAME="$OPTARG"
-    ;;
-    i) PACKAGEDIR="$OPTARG"
-    ;;
-    u) SERVICEUSER="$OPTARG"
-    ;;
-    e) EXECUTABLENAME="$OPTARG"
+    a) ARCH="$OPTARG"
     ;;
     s) SYSTEMD_DB="false"
     ;;
@@ -40,26 +26,8 @@ while getopts ":r:o:d:a:p:i:u:e:s" opt; do
   esac
 done
 if [ -z "$REF" ]; then REF=$(curl -s https://api.github.com/repos/dani-garcia/vaultwarden/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c 1-); fi
-ARCH=$ARCH_DIR
-if [[ "$ARCH" =~ ^arm ]]; then ARCH="armhf"; fi
 
-VAULTWARDEN_DEPS="libc6, libgcc-s1"
-# Setup libssl dependency correctly depending on Debian version.
-# Also use this opportunity to exit if the user is trying to create a package for anything besides bullseye and bookworm
-case $OS_VERSION_NAME in
-  bullseye)
-    VAULTWARDEN_DEPS="$VAULTWARDEN_DEPS, libssl1.1"
-    ;;
-
-  bookworm)
-    VAULTWARDEN_DEPS="$VAULTWARDEN_DEPS, libssl3"
-    ;;
-
-  *)
-    echo "ERROR: vaultwarden-debian only supports building packages for Debian Bullseye and Bookworm currently"
-    exit 1
-    ;;
-esac
+VAULTWARDEN_DEPS="libc6, libgcc-s1, libssl1.1"
 
 # mysql and postgresql need additional libraries. Sqlite support is built into the vaultwarden binary
 case $DB_TYPE in
@@ -74,71 +42,12 @@ case $DB_TYPE in
     ;;
 esac
 
-# Clone vaultwarden
-if [ ! -d "$SRC" ]; then
-  git clone https://github.com/dani-garcia/vaultwarden.git "$SRC"
-fi
-pushd "$SRC" || exit
-CREF="$(git branch | grep "\*" | cut -d ' ' -f2)"
-if [ "$CREF" != "$REF" ]; then
-  git fetch || exit
-  git checkout "$REF" --force || exit
-else
-  git pull || exit
-fi
-git clean -d -f || exit
-popd || exit
-
-DEBIANDIR="$SRC/debian"
-mkdir -p "$DEBIANDIR"
-
-SEDCOMMANDS="
-s/@@PACKAGENAME@@/$PACKAGENAME/g
-s/@@PACKAGEDIR@@/$PACKAGEDIR/g
-s/@@SERVICEUSER@@/$SERVICEUSER/g
-s/@@EXECUTABLENAME@@/$EXECUTABLENAME/g
-"
-
-# Prepare EnvFile
-CONFIG="$DEBIANDIR/config.env"
-cp "$SRC/.env.template" "$CONFIG"
-sed -i "s#\# DATA_FOLDER=data#DATA_FOLDER=/var/lib/$PACKAGEDIR#" "$CONFIG"
-sed -i "s#\# WEB_VAULT_FOLDER=web-vault/#WEB_VAULT_FOLDER=/usr/share/$PACKAGEDIR/web-vault/#" "$CONFIG"
-sed -i "s/Uncomment any of the following lines to change the defaults/Uncomment any of the following lines to change the defaults\n\n## Warning\n## The default systemd-unit does not allow any custom directories.\n## Be sure to check if the service has appropriate permissions before you set custom paths./g" "$CONFIG"
-
-# Prepare conffiles
-sed conffiles.dist > "$DEBIANDIR/conffiles" -f <( echo "$SEDCOMMANDS" ) || exit
-chmod 644 "$DEBIANDIR/conffiles"
-
-# Prepare postinst
-sed postinst.dist > "$DEBIANDIR/postinst" -f <( echo "$SEDCOMMANDS" ) || exit
-chmod 755 "$DEBIANDIR/postinst"
-
-# Prepare postrm
-sed postrm.dist > "$DEBIANDIR/postrm" -f <( echo "$SEDCOMMANDS" ) || exit
-chmod 755 "$DEBIANDIR/postrm"
-
-# Prepare prerm
-sed prerm.dist > "$DEBIANDIR/prerm" -f <( echo "$SEDCOMMANDS" ) || exit
-chmod 755 "$DEBIANDIR/prerm"
-
+DEBIANDIR="$DIR/debian"
 mkdir -p "$DST"
-
-# Prepare Dockerfile
-pushd "$SRC/docker" || exit
-make
-popd || exit
-sed "$DIR/patch/$ARCH_DIR/Dockerfile.patch" -f <( echo "$SEDCOMMANDS" ) | \
-patch "$SRC/docker/Dockerfile.debian" --verbose -o "$DIR/Dockerfile" || \
-exit
-
-sed -E "s/(FROM[[:space:]]*docker.io\/library\/rust:[^-]+)[^[:space:]]+(.+)/\1-${OS_VERSION_NAME}\2/g" -i "$DIR/Dockerfile"
-sed -E "s/(FROM[[:space:]]*docker.io\/library\/debian:)[^-]+(-.+)/\1${OS_VERSION_NAME}\2/g" -i "$DIR/Dockerfile"
 
 # Prepare Controlfile
 CONTROL="$DEBIANDIR/control"
 cp "$DIR/control.dist" "$CONTROL"
-sed -i "s/@@PACKAGENAME@@/$PACKAGENAME/g" "$CONTROL"
 sed -i "s/@@VAULTWARDEN_DEPS@@/$VAULTWARDEN_DEPS/g" "$CONTROL"
 sed -i "s/Version:.*/Version: $REF-1/" "$CONTROL"
 sed -i "s/Architecture:.*/Architecture: $ARCH/" "$CONTROL"
@@ -147,25 +56,15 @@ if [ -n "$RECOMMENDS" ]; then
 fi
 
 # Prepare Systemd-unit
-SYSTEMD_UNIT="$DEBIANDIR/$EXECUTABLENAME.service"
-sed "$DIR/service.dist" > "$SYSTEMD_UNIT" -f <( echo "$SEDCOMMANDS" ) || exit
+SYSTEMD_UNIT="$DEBIANDIR/vaultwarden.service"
 if [ "$SYSTEMD_DB" = true ] && [ "$DB_TYPE" = "mysql" ]; then
   sed -i "s/After=network.target/After=network.target mysqld.service\nRequires=mysqld.service/g" "$SYSTEMD_UNIT"
 elif [ "$SYSTEMD_DB" = true ] && [ "$DB_TYPE" = "postgresql" ]; then
   sed -i "s/After=network.target/After=network.target postgresql.service\nRequires=postgresql.service/g" "$SYSTEMD_UNIT"
 fi
 
-# Prepare sysusers
-sed sysusers.conf > "$DEBIANDIR/sysusers.conf" -f <( echo "$SEDCOMMANDS" ) || exit
-chmod 644 "$DEBIANDIR/sysusers.conf"
-
-# Prepare tmpfiles
-sed tmpfiles.conf > "$DEBIANDIR/tmpfiles.conf" -f <( echo "$SEDCOMMANDS" ) || exit
-chmod 644 "$DEBIANDIR/tmpfiles.conf"
-
-echo "[INFO] docker build -t vaultwarden-deb $DIR --build-arg DB=$DB_TYPE"
-docker build -t vaultwarden-deb "$SRC" --build-arg DB="$DB_TYPE" --target dpkg -f "$DIR/Dockerfile"
-
-CID=$(docker run -d vaultwarden-deb)
-docker cp "$CID:/outdir/${PACKAGEDIR}.deb" "$DST/${PACKAGEDIR}-${OS_VERSION_NAME}-${REF}-${DB_TYPE}-${ARCH_DIR}.deb"
-docker rm "$CID"
+out=$(mktemp -d)
+docker build -t vaultwarden-deb --build-arg ref="$REF" .
+docker run --rm -v "$out:/outdir" vaultwarden-deb
+mv "$out/vaultwarden.deb" "$DST/vaultwarden-${REF}-${DB_TYPE}-${ARCH}.deb"
+rmdir "$out"
